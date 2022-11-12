bc8c7e3a118893cc47cd7d9e9767e118206113f4
a4six <beverly_csu@mail.ru>
2022-11-12 17:48:56 +0500

    sasd

--- Pandas/Data Clear/feature_engineering.py
+++ Pandas/Data Clear/feature_engineering.py
@@ -1,12 +1,25 @@
 import pandas as pd
-df = pd.read_csv('GooglePlayStore_wild.csv')
+from data_cleaning import *
 
 # Очистка данных из первого задания
 
 # Замени тип данных на дробное число (float) для цен приложений ('Price')
+def set_price(price):
+    if price[0] == '$':
+        return float(price[1:])
+    return 0
 
+df['Price'] = df['Price'].apply(set_price)
 # Вычисли, сколько долларов разработчики заработали на каждом платном приложении
+def set_installs(installs):
+    if installs[-1] == '+':
+        installs = installs[:-1]
+    installs = installs.replace(',', '')
+    return int(installs)
 
+df['Installs'] = df['Installs'].apply(set_installs)
+
+df['Profit'] = df['Installs'] * df['Price']
 # Чему равен максимальный доход ('Profit') среди платных приложений (Type == 'Paid')?
 
 # Создай новый столбец, в котором будет храниться количество жанров. Назови его 'Number of genres'
