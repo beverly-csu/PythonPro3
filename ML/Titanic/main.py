ef516a2d3ffa69d724836d5069e87d371fae80b5
a4six <beverly_csu@mail.ru>
2022-12-17 12:01:01 +0500

    Update main.py

--- ML/Titanic/main.py
+++ ML/Titanic/main.py
@@ -4,4 +4,21 @@ import pandas as pd
 df = pd.read_csv('titanic.csv')
 
 df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], inplace=True, axis=1)
-df.info()
+
+age_1 = df[df['Pclass'] == 1]['Age'].median()
+age_2 = df[df['Pclass'] == 2]['Age'].median()
+age_3 = df[df['Pclass'] == 3]['Age'].median()
+
+def fill_age(row):
+    if pd.isnull(row['Age']):
+        if row['Pclass'] == 1:
+            return age_1
+        elif row['Pclass'] == 2:
+            return age_2
+        else:
+            return age_3
+    else:
+        return row['Age']
+
+df['Age'] = df.apply(fill_age, axis=1)
+# df.info()
