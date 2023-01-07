b199df43d568eb242f53fc962b5ec09cfe9edcef
a4six <beverly_csu@mail.ru>
2023-01-07 15:53:49 +0500

    ss

---
+++ ML/DigitalEdu/digital_edu.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('train.csv')
+df.info()
+
+def debug(name):
+    print(df[name].value_counts())
+
+def detect_english(langs):
+    langs = langs.split(';')
+    if 'English' in langs:
+        return 1
+    else:
+        return 0
+
+df['english'] = df['langs'].apply(detect_english)
--- ML/Titanic/main.py
+++ ML/Titanic/main.py
@@ -32,8 +32,26 @@ def fill_sex(sex):
 
 df['Sex'] = df['Sex'].apply(fill_sex)
 
-# print(df['Embarked'].value_counts())
 df[list(pd.get_dummies(df['Embarked']).columns)] = pd.get_dummies(df['Embarked'])
 df.drop('Embarked', axis=1, inplace=True)
 
-df.info()
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+x = df.drop('Survived', axis=1)
+y = df['Survived']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(x_train, y_train)
+
+y_pred = classifier.predict(x_test)
+percent = accuracy_score(y_test, y_pred) * 100
+print(f"Успешность модели: {round(percent, 2)}%")
