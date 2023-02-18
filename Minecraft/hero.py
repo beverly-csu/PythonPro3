98552f46f67365f87fbbc3a4d2ad0fb553b767a1
a4six <beverly_csu@mail.ru>
2023-02-18 17:10:43 +0500

    Update hero.py

--- Minecraft/hero.py
+++ Minecraft/hero.py
@@ -17,6 +17,7 @@ KEY_DESTROY = 'v'
 class Hero:
     def __init__(self, pos, land):
         self.land = land
+        self.mode = True
         self.hero = loader.loadModel('teapot')
         self.hero.setPos(pos)
         self.hero.setColor((0, 1, 0, 1))
@@ -44,5 +45,70 @@ class Hero:
         else:
             self.cameraBind()
 
+    def check_dir(self, angle):
+        if angle >= 0 and angle <= 20:
+            return (0, -1)
+        elif angle <= 65:
+            return (1, -1)
+        elif angle <= 110:
+            return (1, 0)
+        elif angle <= 155:
+            return (1, 1)
+        elif angle <= 200:
+            return (0, 1)
+        elif angle <= 245:
+            return (-1, 1)
+        elif angle <= 290:
+            return (-1, 0)
+        elif angle <= 335:
+            return (-1, -1)
+        else:
+            return (0, -1)
+
+    def look_at(self, angle):
+        x_from = int(self.hero.getX())
+        y_from = int(self.hero.getY())
+        z_from = int(self.hero.getZ())
+
+        dx, dy = self.check_dir(angle)
+
+        x_to = x_from + dx
+        y_to = y_from + dy
+
+        return (x_to, y_to, z_from)
+
+    def just_move(self, angle):
+        pos = self.look_at(angle)
+        self.hero.setPos(pos)
+
+    def move_to(self, angle):
+        if self.mode:
+            self.just_move(angle)
+
+    def forward(self):
+        angle = self.hero.getH() % 360
+        self.move_to(angle)
+
+    def left(self):
+        angle = (self.hero.getH() + 90) % 360
+        self.move_to(angle)
+    
+    def right(self):
+        angle = (self.hero.getH() + 270) % 360
+        self.move_to(angle)
+    
+    def back(self):
+        angle = (self.hero.getH() + 180) % 360
+        self.move_to(angle)
+
     def acceptEvents(self):
         base.accept(KEY_CHANGE_CAMERA, self.cameraChange)
+
+        base.accept(KEY_FORWARD, self.forward)
+        base.accept(KEY_FORWARD + '-repeat', self.forward)
+        base.accept(KEY_BACK, self.back)
+        base.accept(KEY_BACK + '-repeat', self.back)
+        base.accept(KEY_LEFT, self.left)
+        base.accept(KEY_LEFT + '-repeat', self.left)
+        base.accept(KEY_RIGHT, self.right)
+        base.accept(KEY_RIGHT + '-repeat', self.right)
